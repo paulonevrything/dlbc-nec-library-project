063f25366e82d6517bedd57a4eff935566492f53
Paul Olabisi <pauloolabisi@gmail.com>
2025-08-13 21:14:02 +0100

    updated frontend app name

--- dlbc-nec-library-frontend/src/app/app.ts
+++ dlbc-nec-library-frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('dlbc-nec-library');
+  protected readonly title = signal('dlbc-nec-library-frontend');
 }
